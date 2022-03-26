23847a2e21cda51de1493b7cc23d78f533aa03fe
hoatranv <tranvanhoactk34@gmail.com>
2022-03-26 22:48:01 +0700

    Introduce springboot-common-app to adding the common things in spring world

---
+++ common-apps/springboot-common-app/src/main/java/com/hoatv/springboot/common/advices/DefaultExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.hoatv.springboot.common.advices;
+
+import com.hoatv.fwk.common.exceptions.AppException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@ControllerAdvice
+public class DefaultExceptionHandler {
+
+    @ResponseBody
+    @ResponseStatus(BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.badRequest()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .body(errors);
+    }
+
+    @ExceptionHandler(value= {AppException.class})
+    protected ResponseEntity<Object> handleAppException(RuntimeException ex, WebRequest request) {
+        String responseMessage = String.format("{\"message\": \"%s\"}", ex.getMessage());
+        request.setAttribute("javax.servlet.error.exception", ex, 0);
+        return ResponseEntity.internalServerError()
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .body(responseMessage);
+    }
+
+}
---
+++ common-apps/springboot-common-app/src/main/java/com/hoatv/springboot/common/validation/ValueOfEnumValidator.java
@@ -0,0 +1,36 @@
+package com.hoatv.springboot.common.validation;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ValueOfEnumValidator implements ConstraintValidator<ValueOfEnum, CharSequence> {
+    private List<String> acceptedValues;
+    private String message;
+
+    @Override
+    public void initialize(ValueOfEnum annotation) {
+        message = annotation.message();
+        acceptedValues = Stream.of(annotation.value().getEnumConstants())
+            .map(Enum::name)
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isValid(CharSequence value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true;
+        }
+
+        if (StringUtils.isEmpty(message)) {
+            context.disableDefaultConstraintViolation();
+            String messageTemplate = "Must be any of values " + acceptedValues;
+            context.buildConstraintViolationWithTemplate(messageTemplate).addConstraintViolation();
+        }
+        return acceptedValues.contains(value.toString());
+    }
+}
