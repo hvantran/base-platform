86273da4a776047a3884dc55ef48a20ceb703bbb
Hoa Tran Van <hoa.tranvan@nashtechglobal.com>
2022-04-08 14:53:59 +0700

    Introduce base initial configuration in springboot-common-app

---
+++ common-apps/springboot-common-app/src/main/java/com/hoatv/springboot/common/configurations/InitializeConfigurations.java
@@ -0,0 +1,113 @@
+package com.hoatv.springboot.common.configurations;
+
+import com.hoatv.fwk.common.services.HttpClientFactory;
+import com.hoatv.fwk.common.ultilities.ObjectUtils;
+import com.hoatv.metric.mgmt.annotations.MetricProvider;
+import com.hoatv.metric.mgmt.annotations.MetricRegistry;
+import com.hoatv.metric.mgmt.services.MetricMgmtService;
+import com.hoatv.metric.mgmt.services.MetricProviderRegistry;
+import com.hoatv.task.mgmt.annotations.SchedulePoolSettings;
+import com.hoatv.task.mgmt.entities.TaskCollection;
+import com.hoatv.task.mgmt.services.ScheduleTaskMgmtService;
+import com.hoatv.task.mgmt.services.ScheduleTaskRegistryService;
+import com.hoatv.task.mgmt.services.TaskFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Configuration
+public class InitializeConfigurations {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(InitializeConfigurations.class);
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build();
+    }
+
+    @Bean
+    public MetricProviderRegistry getMetricProviderRegistry() {
+        return new MetricProviderRegistry();
+    }
+
+    @Bean
+    public MetricMgmtService getMetricMgmtService(MetricProviderRegistry metricProviderRegistry) {
+        return new MetricMgmtService(metricProviderRegistry);
+    }
+
+    @Bean
+    public ServletListenerRegistrationBean<ServletContextListener> servletListener() {
+        ServletListenerRegistrationBean<ServletContextListener> srb = new ServletListenerRegistrationBean<>();
+        srb.setListener(new ApplicationServletContextListener());
+        return srb;
+    }
+
+    @Bean(destroyMethod = "destroy")
+    public ScheduleTaskRegistryService getScheduleTaskRegistryService() {
+        return new ScheduleTaskRegistryService();
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            LOGGER.info("Let's inspect the beans provided by Spring Boot");
+            String[] metricProviderBeanNames = ctx.getBeanNamesForAnnotation(MetricProvider.class);
+            String[] metricRegistryBeanNames = ctx.getBeanNamesForAnnotation(MetricRegistry.class);
+            String[] schedulePoolSettingBeanNames = ctx.getBeanNamesForAnnotation(SchedulePoolSettings.class);
+
+            List<Object> metricProviders = Stream.of(metricProviderBeanNames).map(ctx::getBean).collect(Collectors.toList());
+            List<Object> metricRegistries = Stream.of(metricRegistryBeanNames).map(ctx::getBean).collect(Collectors.toList());
+            List<Object> poolSettings = Stream.of(schedulePoolSettingBeanNames).map(ctx::getBean).collect(Collectors.toList());
+            ObjectUtils.checkThenThrow(metricRegistries.size() != 1, "Required at least one of Metric Registry annotation");
+            Object metricRegistry = metricRegistries.stream().findFirst().orElseThrow();
+            ObjectUtils.checkThenThrow(!(metricRegistry instanceof MetricProviderRegistry), "This must be an instance of MetricProviderRegistry");
+            MetricProviderRegistry metricProviderRegistry = (MetricProviderRegistry) metricRegistry;
+            metricProviderRegistry.loadFromObjects(metricProviders);
+
+            ScheduleTaskRegistryService scheduleTaskExecutorService = ctx.getBean(ScheduleTaskRegistryService.class);
+
+            poolSettings.forEach(applicationObj -> {
+                SchedulePoolSettings schedulePoolSettings = applicationObj.getClass().getAnnotation(SchedulePoolSettings.class);
+                ScheduleTaskMgmtService taskMgmtExecutorV1 = TaskFactory.INSTANCE.newScheduleTaskMgmtService(schedulePoolSettings);
+
+                scheduleTaskExecutorService.register(schedulePoolSettings.application(), taskMgmtExecutorV1);
+                TaskCollection taskCollection = TaskCollection.fromObject(applicationObj);
+                taskMgmtExecutorV1.scheduleTasks(taskCollection, 5, TimeUnit.SECONDS);
+            });
+        };
+    }
+
+    public static class ApplicationServletContextListener implements ServletContextListener {
+
+        @Override
+        public void contextDestroyed(ServletContextEvent event) {
+            LOGGER.info("Application context is destroyed");
+            HttpClientFactory.INSTANCE.destroy();
+            TaskFactory.INSTANCE.destroy();
+        }
+
+        @Override
+        public void contextInitialized(ServletContextEvent event) {
+            LOGGER.info("Application context is initialized");
+        }
+    }
+}
