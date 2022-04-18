95c5aa21aa62f1f49a5fb106424860018873268a
hoatranv <tranvanhoactk34@gmail.com>
2022-04-19 00:18:13 +0700

    Make inherited for annotations

--- common-apps/system-health-statistics/src/main/java/com/hoatv/system/health/metrics/SystemHealth.java
+++ common-apps/system-health-statistics/src/main/java/com/hoatv/system/health/metrics/SystemHealth.java
@@ -5,13 +5,11 @@ import com.hoatv.metric.mgmt.annotations.Metric;
 import com.hoatv.metric.mgmt.annotations.MetricProvider;
 import com.hoatv.metric.mgmt.entities.SimpleValue;
 import com.sun.management.OperatingSystemMXBean;
-import org.springframework.stereotype.Component;
 
 import java.lang.management.ManagementFactory;
 
 import static com.hoatv.fwk.common.constants.Constants.SYSTEM_APPLICATION;
 
-@Component
 @MetricProvider(application = SYSTEM_APPLICATION, category = SYSTEM_APPLICATION)
 public class SystemHealth {
     private final SimpleValue initHeapSpace = new SimpleValue(0);
--- task-management/src/main/java/com/hoatv/task/mgmt/annotations/ScheduleTask.java
+++ task-management/src/main/java/com/hoatv/task/mgmt/annotations/ScheduleTask.java
@@ -1,13 +1,9 @@
 package com.hoatv.task.mgmt.annotations;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
-/**
- *
- */
+
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface ScheduleTask {
