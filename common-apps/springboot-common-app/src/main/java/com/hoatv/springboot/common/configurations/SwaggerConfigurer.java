62f329850504f7f1c7134373f16ef11de821e9e7
hoatranv <tranvanhoactk34@gmail.com>
2022-05-09 16:05:02 +0700

    Replace springfox by springdoc-swagger-ui

--- common-apps/springboot-common-app/src/main/java/com/hoatv/springboot/common/configurations/SwaggerConfigurer.java
+++ common-apps/springboot-common-app/src/main/java/com/hoatv/springboot/common/configurations/SwaggerConfigurer.java
@@ -1,14 +1,14 @@
 package com.hoatv.springboot.common.configurations;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 @Profile({"dev", "test"})
@@ -17,12 +17,15 @@ public class SwaggerConfigurer {
     private static final Logger LOGGER = LoggerFactory.getLogger(SwaggerConfigurer.class);
 
     @Bean
-    public Docket api() {
-        LOGGER.info("Initialize Swagger");
-        return new Docket(DocumentationType.SWAGGER_2)
-            .select()
-            .apis(RequestHandlerSelectors.basePackage("com.hoatv"))
-            .paths(PathSelectors.any())
-            .build();
+    public OpenAPI springShopOpenAPI() {
+        LOGGER.info("Configuring Swagger");
+        return new OpenAPI()
+                .info(new Info().title("My application API")
+                        .description("API documentation application")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("API Documentation")
+                        .url("https://github.com/hvantran/project-management"));
     }
 }
