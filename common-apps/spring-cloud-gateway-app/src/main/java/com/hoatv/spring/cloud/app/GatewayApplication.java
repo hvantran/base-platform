aa42e9ce81e8a53d21c50161066fc4fd0a90a342
Hoa Tran Van <tranvanhoactk34@gmail.com>
2022-09-04 07:04:01 +0700

    Update pom.xml file

--- common-apps/spring-cloud-gateway-app/src/main/java/com/hoatv/spring/cloud/app/GatewayApplication.java
+++ common-apps/spring-cloud-gateway-app/src/main/java/com/hoatv/spring/cloud/app/GatewayApplication.java
@@ -2,8 +2,10 @@ package com.hoatv.spring.cloud.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
+@EnableWebFluxSecurity
 public class GatewayApplication {
 
     public static void main(String[] args) {
