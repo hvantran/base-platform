3eb80b0e0880d91d44ecbd63cf8781291e347e5a
Hoa Tran <tranvanhoactk34@gmail.com>
2023-04-07 15:32:29 +0700

    Migrate action manager to docker environment

--- action-management-app/src/main/java/com/hoatv/action/manager/ActionManagerApplication.java
+++ action-management-app/src/main/java/com/hoatv/action/manager/ActionManagerApplication.java
@@ -1,22 +1,10 @@
 package com.hoatv.action.manager;
 
-import com.hoatv.action.manager.collections.ActionDocument;
-import com.hoatv.action.manager.repositories.ActionDocumentRepository;
-import com.hoatv.action.manager.services.JobResult;
-import com.hoatv.action.manager.services.ScriptEngineService;
-import org.apache.commons.io.FileUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-import java.io.File;
-import java.util.Collections;
-import java.util.List;
 
 @Configuration
 @EnableAutoConfiguration
@@ -24,6 +12,10 @@ import java.util.List;
 @EnableMongoRepositories
 public class ActionManagerApplication {
     public static void main (String[] args) {
+        try{
         SpringApplication.run(ActionManagerApplication.class, args);
+    } catch (Throwable e) {
+        e.printStackTrace();
+    }
     }
 }
