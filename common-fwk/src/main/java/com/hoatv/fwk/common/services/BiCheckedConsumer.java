cf22fa38642c586416889aba8c1dce27f88a869b
Hoa Tran <tranvanhoactk34@gmail.com>
2023-05-02 00:52:09 +0700

    Add BiCheckedConsummer

---
+++ common-fwk/src/main/java/com/hoatv/fwk/common/services/BiCheckedConsumer.java
@@ -0,0 +1,19 @@
+package com.hoatv.fwk.common.services;
+
+import com.hoatv.fwk.common.exceptions.AppException;
+import java.util.Objects;
+import java.util.function.BiConsumer;
+
+@FunctionalInterface
+public interface BiCheckedConsumer<T, U> extends BiConsumer<T, U> {
+
+    default void accept (T input, U inputArg) {
+        try {
+            acceptThrows(input, inputArg);
+        } catch (Exception exception) {
+            throw new AppException(exception);
+        }
+    }
+
+    void acceptThrows(T input, U inputArg) throws Exception;
+}
