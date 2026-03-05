92f0a9c6c4c6d50235a4ad64374ce6c21d7e419b
hvantran <tranvanhoactk34@gmail.com>
2026-03-06 00:09:47 +0700

    [base-platform] Restore springboot-common-app with servlet component exclusion
    Restoring springboot-common-app dependency but properly excluding servlet components:
    - Added jakarta.servlet-api exclusion in pom.xml
    - Use @ComponentScan excludeFilters to prevent InitializeConfigurations from loading
    - InitializeConfigurations contains ServletListenerRegistrationBean incompatible with WebFlux
    - Gateway needs other parts of springboot-common-app (logging config, etc.)

--- common-apps/spring-cloud-gateway-app/src/main/java/com/hoatv/spring/cloud/app/GatewayApplication.java
+++ common-apps/spring-cloud-gateway-app/src/main/java/com/hoatv/spring/cloud/app/GatewayApplication.java
@@ -1,9 +1,25 @@
 package com.hoatv.spring.cloud.app;
 
+import com.hoatv.springboot.common.configurations.InitializeConfigurations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
+/**
+ * Spring Cloud Gateway Application (Reactive WebFlux)
+ * 
+ * Note: Excludes InitializeConfigurations from springboot-common-app because it contains
+ * servlet-based components (ServletListenerRegistrationBean) incompatible with reactive WebFlux.
+ */
 @SpringBootApplication
+@ComponentScan(
+    basePackages = {"com.hoatv"},
+    excludeFilters = @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = {InitializeConfigurations.class}
+    )
+)
 public class GatewayApplication {
 
     public static void main(String[] args) {
